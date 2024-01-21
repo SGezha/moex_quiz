dd472fb98c57d1c8f306231181701329e8c65d86
Sasha <sasha-gezha1@mail.ru>
2024-01-22 01:35:20 +0300

    start

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-import { styles } from './assets.js'
+// import { styles } from './assets.js'
 import { data } from './quiz.js'
 import { createApp } from './vue.js'
+import widget from './widget.vue?raw'
+import styles from './style.css?raw'
 
 class MessageWidget {
   constructor(position = 'bottom-right') {
@@ -17,8 +19,8 @@ class MessageWidget {
   getPosition(position) {
     const [vertical, horizontal] = position.split('-')
     return {
-      [vertical]: '30px',
-      [horizontal]: '30px'
+      [vertical]: '40px',
+      [horizontal]: '100px'
     }
   }
 
@@ -39,39 +41,7 @@ class MessageWidget {
   }
 
   createWidgetContent() {
-    this.widgetContainer.innerHTML = `
-        <div id="quiz_widget">
-          <Transition name="fade">
-            <div v-if="open" class="widget__container">
-              <div class="message__container">
-                <div v-html="data[currentId].text" class="message_title"></div>
-                <div class="message_actions">
-                  <button @click="changeSlide(btn.to)" v-for="btn in data[currentId].actions" :key="btn.title">
-                    {{ btn.title }}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Transition>
-          <button @click="toggleOpen()" class="button__container">
-            <Transition name="fade">
-              <span v-if="!open" class="widget__icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-question-mark" width="32" height="32" stroke-width="2" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4"></path>
-                    <path d="M12 19l0 .01"></path>
-                </svg>
-              </span>
-              <span v-else class="widget__icon">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
-                      <line x1="18" y1="6" x2="6" y2="18"></line>
-                      <line x1="6" y1="6" x2="18" y2="18"></line>
-                  </svg>
-              </span>
-            </Transition>
-          </button>
-        </div>
-    `
+    this.widgetContainer.innerHTML = widget.replace('<template>', '').replace('</template>', '')
   }
 
   injectStyles() {
@@ -86,16 +56,24 @@ class MessageWidget {
       data() {
         return {
           open: false,
-          currentId: 0,
-          data: data
+          data: data,
+          history: [
+            {
+              id: 'idle',
+              answer: null
+            }
+          ]
         }
       },
       methods: {
         toggleOpen() {
           this.open = !this.open
         },
-        changeSlide(id) {
-          this.currentId = id
+        nextQuest(id) {
+          this.history.push({
+            id,
+            answer: null
+          })
         }
       }
     }).mount('#quiz_widget')
---
+++ widget.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="quiz_widget">
+    <Transition name="chat">
+      <div v-if="open" class="widget__container">
+        <div class="message__container">
+          <div v-for="state in history" class="msg_list">
+            <div class="message__author">
+              {{ data.states[state.id].author }}
+            </div>
+            <div class="messages__block">
+              <div
+                v-for="msg in data.states[state.id].messages"
+                :key="msg"
+                v-html="msg.text"
+                class="msg"
+              ></div>
+            </div>
+            <div class="message_actions">
+              <button
+                @click="nextQuest(value.target)"
+                v-for="value in data.states[state.id].on"
+                :key="value.label"
+              >
+                {{ value.label }}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+    <button @click="toggleOpen()" class="button__container">
+      <Transition name="fade">
+        <span v-if="!open" class="widget__icon">
+          <svg
+            width="39"
+            height="22"
+            viewBox="0 0 39 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="13.9314"
+              y="0.880371"
+              width="25.0672"
+              height="9.5769"
+              fill="#FF0508"
+            />
+            <rect
+              x="0.324951"
+              y="9.7229"
+              width="38.6751"
+              height="11.787"
+              fill="#FF0508"
+            />
+          </svg>
+        </span>
+        <span v-else class="widget__icon">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18 6L6 18"
+              stroke="#FF0508"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M6 6L18 18"
+              stroke="#FF0508"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </span>
+      </Transition>
+    </button>
+  </div>
+</template>
